1af333b2cce4d68097140e71e88cfc8fd84abc42
omerbspnr <baspinaromer1996@gmail.com>
2020-04-24 12:44:37 +0300

    java için farklı singleton örnekleri

---
+++ singleton/java/eagerDatabase.java
@@ -0,0 +1,5 @@
+package Singleton;
+public class eagerDatabase {
+    public static final eagerDatabase DATABASE = new eagerDatabase();
+    private eagerDatabase(){}
+}
---
+++ singleton/java/enumDatabase.java
@@ -0,0 +1,6 @@
+package Singleton;
+
+public enum enumDatabase {
+    INSTANCE;
+    
+}
