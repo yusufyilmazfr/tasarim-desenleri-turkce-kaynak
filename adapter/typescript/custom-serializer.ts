ca990bfd70cb85fe3a624717f7e37b58574c511e
Eren Kurnaz <h.erenkurnaz@icloud.com>
2022-05-14 13:50:53 +0300

    Add missing comments to adapter

--- adapter/typescript/custom-serializer.ts
+++ adapter/typescript/custom-serializer.ts
@@ -1,3 +1,5 @@
+// Sisteme daha sonradan dahil olan yapı.
+// Sistemin kullandığı arayüzü uygulamamaktadır.
 export class CustomSerializer {
   public serialize(obj: object): string {
     // Gelen nesneyi serilize etmek için temel operasyonel işlemler..
--- adapter/typescript/json-serializer.interface.ts
+++ adapter/typescript/json-serializer.interface.ts
@@ -1,5 +1,5 @@
+// UML diyagramındaki Target'a denk gelmektedir.
+// Sistem bu arayüzü uygulayarak çalışmaktadır.
 export interface IJsonSerializer<T extends object = object> {
-  // UML diyagramındaki Target'a denk gelmektedir.
-  // Sistem bu arayüzü uygulayarak çalışmaktadır.
   serializeObject(obj: T): string;
 }
