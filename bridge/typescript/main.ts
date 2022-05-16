97aecfd9a817260a834d75c99faf76efd76cfc8d
Eren Kurnaz <h.erenkurnaz@icloud.com>
2022-05-16 20:47:10 +0300

    Fix email message in bridge example

--- bridge/typescript/main.ts
+++ bridge/typescript/main.ts
@@ -1,6 +1,7 @@
 import {TextMessageSender} from "./text-message-sender";
 import {TextMessage} from "./text-message";
 import {EmailMessageSender} from "./email-message-sender";
+import {EmailMessage} from "./email-message";
 
 function main() {
   const textMessageSender = new TextMessageSender();
@@ -8,6 +9,6 @@ function main() {
   textMessage.send(); // output: TextMessageSender: Sending text message...
 
   const emailMessageSender = new EmailMessageSender();
-  const emailMessage = new TextMessage(emailMessageSender);
+  const emailMessage = new EmailMessage(emailMessageSender);
   emailMessage.send(); // output: EmailMessageSender: Sending email message...
 }
