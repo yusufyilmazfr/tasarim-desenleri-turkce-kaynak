cca029f234d5004c5a3e12412c494c913e0af25f
okmsbun <bun.53@hotmail.com>
2024-02-22 14:53:57 +0300

    factory alternative added

--- factory/dart/i_notify.dart
+++ factory/dart/i_notify.dart
@@ -1,5 +1,20 @@
+import 'mail_notify.dart';
+import 'sms_notify.dart';
 import 'user.dart';
 
 abstract class INotify {
   void sendNotification(User user);
+
+  // Alternatif
+  //? Ayrıca bakınız: https://dart.dev/language/constructors#factory-constructors
+  factory INotify.createNotify(String type) {
+    switch (type) {
+      case "MAIL":
+        return MailNotify();
+      case "SMS":
+        return SmsNotify();
+      default:
+        throw "Geçersiz bildirim türü.";
+    }
+  }
 }
--- factory/dart/main.dart
+++ factory/dart/main.dart
@@ -3,13 +3,12 @@ import 'notify_factory.dart';
 import 'user.dart';
 
 void main() {
-  //? Bu örnek diğer dillerin örnekleri ile tamamen aynı olsun diye bu şekilde yazıldı. Sorunsuz çalışır.
-  //? Dart dilinde bu şekilde kullanmak yerine 'factory' anahtar kelimesi kullanılabilir.
-  //? Ayrıca bakınız: https://dart.dev/language/constructors#factory-constructors
-
   final NotifyFactory notifyFactory = NotifyFactory();
 
   final INotify? notify = notifyFactory.createNotify("MAIL");
-
   notify?.sendNotification(User());
+
+  // Alternatif
+  final INotify notify2 = INotify.createNotify("SMS");
+  notify2.sendNotification(User());
 }
